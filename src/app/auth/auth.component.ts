c62862d00ebda148319015d8faaea0835f0a43bd
Emmanuel Asamoah Botwe <noelnuel44@gmail.com>
2022-08-19 13:36:12 +0000

    updated callback

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -12,7 +12,7 @@ export class AuthCompnent implements OnInit {
   //   https://twitter.com/i/oauth2/authorize?response_type=code&client_id=aXpOOWZkSm1JVHJ3S0o3YkJhSmI6MTpjaQ&redirect_uri=http://localhost:4200m&scope=tweet.read%20users.read%20follows.read%20offline.access&state=foo&code_challenge=bar&code_challenge_method=plain
   // https://www.example.com/?state=state&code=eWQ0SFlqWnVBUVFaMl84TWg1bk0zQnBZMnYzSzJsc3phYUFrd3NSeGpkTGhuOjE2NjA5MDI1OTE0MDY6MTowOmFjOjE
   redirectUrl = 'http://localhost:4200/callback'
-  loginUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=aXpOOWZkSm1JVHJ3S0o3YkJhSmI6MTpjaQ&redirect_uri=https://twitterfactchecker.herokuapp.com&scope=tweet.read%20users.read%20follows.read%20follows.write&state=state&code_challenge=challenge&code_challenge_method=plain`;
+  loginUrl = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=aXpOOWZkSm1JVHJ3S0o3YkJhSmI6MTpjaQ&redirect_uri=https://twitterfactchecker.herokuapp.com/callback&scope=tweet.read%20users.read%20follows.read%20follows.write&state=state&code_challenge=challenge&code_challenge_method=plain`;
 // https://twitterfactchecker.herokuapp.com/#/
   constructor(
     private router: Router
